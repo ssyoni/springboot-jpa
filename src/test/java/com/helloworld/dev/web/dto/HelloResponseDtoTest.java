2435790a48b20921921a6d936ffb0b1ee93f22c6
ssyoni <jsy950601@gmail.com>
2021-12-14 19:52:51 +0900

    Initial commit

---
+++ src/test/java/com/helloworld/dev/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,28 @@
+package com.helloworld.dev.web.dto;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    public void test_lombok(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+
+}
