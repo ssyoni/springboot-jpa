1a9c755330e3c03250707975ec85c879ead64a3a
ssyoni <jsy950601@gmail.com>
2021-12-19 21:48:19 +0900

    create update/delete api add querydsl

--- src/main/java/com/helloworld/dev/service/posts/PostsService.java
+++ src/main/java/com/helloworld/dev/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.helloworld.dev.service.posts;
 
 import com.helloworld.dev.domain.posts.Posts;
 import com.helloworld.dev.domain.posts.PostsRepository;
+import com.helloworld.dev.web.dto.PostsListResponseDto;
 import com.helloworld.dev.web.dto.PostsResponseDto;
 import com.helloworld.dev.web.dto.PostsSaveRequestDto;
 import com.helloworld.dev.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -40,4 +44,20 @@ public class PostsService {
         return new PostsResponseDto(entity);
 
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
+    }
+
 }
