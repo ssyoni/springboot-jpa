90412804b9c406aac1187ab265a015b49cef3e3e
ssyoni <jsy950601@gmail.com>
2022-01-12 22:49:32 +0900

    등록 페이지 작업

---
+++ src/main/resources/static/js/app/askManageForm.js
@@ -0,0 +1,19 @@
+var askManageForm = {
+    init : function(){
+        $("#addRadioBtn").on('click',function(){
+            addRadio();
+        })
+    },
+
+    addForm : function(){
+    },
+
+    addAnsForm : function(){
+
+    },
+
+    addRadio : function(){
+
+    },
+    addChkbox : function{}
+}
